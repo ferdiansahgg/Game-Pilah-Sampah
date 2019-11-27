ef00f4ae083237fd2ce5480af5b46d95953cbc81
ferdiansahgg <ferdiayana@gmail.com>
2019-11-28 00:06:31 +0700

    cek1

---
+++ Assets/Scripts/Level2.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Level2 : MonoBehaviour {
+
+    // Use this for initialization
+    float timer = 0;
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if (timer > 5)
+        {
+            Data.score = 0;
+            SceneManager.LoadScene("Level2");
+        }
+    }
+}
